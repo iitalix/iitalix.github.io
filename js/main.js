1d6c13856ffada2f4b62b5c9c6b108ed6dc28952
Lan <nolan.oribello@gmail.com>
2023-12-08 11:01:33 -0800

    styled and configured Spotify iFrame

--- js/main.js
+++ js/main.js
@@ -538,7 +538,7 @@
       });
       $(".spotify-popup").magnificPopup({
         items: {
-          src: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2PfdXmTz7fDNBYGlv1h1Uf" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+          src: '#spotify-iframe',
           type: 'inline',
         }
       });
